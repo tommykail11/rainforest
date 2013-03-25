c680ef10aa5756d1200e3a5cb7cce9e1e5d21bd7
Tommy Kail <tommykail@gmail.com>
2013-03-25 12:08:38 -0400

    Passed one test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,20 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price_in_cents
+  attr_accessible :description, :name, :price_in_cents, :price_in_dollars
 
   validates :description, :name, :presence => true
   validates :price_in_cents, :numericality => {:only_integer => true}
+  validates :price_in_dollars, :numericality => true
+
+
+  def price_in_dollars
+  	if self.price_in_cents?
+  	self.price_in_cents.to_f / 100 # integer => 2 decimal float
+  end
+end
+
+  def price_in_dollars=(amount)
+  	if amount.present?
+			self.price_in_cents = (amount.to_f * 100).to_i
+  	end
+	end
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -15,4 +15,28 @@ class ProductTest < ActiveSupport::TestCase
   	refute @p.valid?
   end
 
+  test "must have a name" do
+  	@p.name = ""
+  	refute @p.valid?
+  end
+
+  test "price_in_cents must be an integer" do
+  	@p.price_in_cents = 1.5
+  	refute @p.valid?
+	end
+
+	test "must have price_in_cents" do
+		@p.price_in_cents = nil
+		refute @p.valid?
+	end
+
+	test "price_in_dollars must be a float" do
+		@p.price_in_dollars = 15.00 && 15
+		assert @p.valid?
+	end
+
+	#test "price_in_dollars to price_in_cents" do
+	#	@p.price_in_dollars = price_in_cents / 100
+	#	assert @p.valid?
+	#end
 end

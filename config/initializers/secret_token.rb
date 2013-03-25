d55e3d411b90de3ab8da65ca0bdc12c993740534
Bitmaker <Bitmaker@Bitmaker08.local>
2013-03-25 08:20:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainforest::Application.config.secret_token = 'd8fd26340ca8a649d4b6047ca9d6e26861d347e16610c6d545f8ecf1c89f98d14c8e8378d11b8d08ad507f6a364d42eaf29804995094e8f83003d0c82ced905b'

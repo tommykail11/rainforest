2cc783244edacdda3e84dcbc8b5c8c56b30d766f
Tommy Kail <tommykail@gmail.com>
2013-03-25 14:30:17 -0400

    Test updates

--- test/factories/products.rb
+++ test/factories/products.rb
@@ -1,4 +1,8 @@
 FactoryGirl.define do 
+	
+
+	# Equivalent of
+	# Product.create(:name => "Shoes, :description => "Nice pants white leather shoes", :price_in_cents => 9900)
 	factory :product do 
 		name "Shoes"
 		description "Nice patent white leather shoes"
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -30,10 +30,39 @@ class ProductTest < ActiveSupport::TestCase
 		refute @p.valid?
 	end
 
+	test "must have a price_in_dollars" do
+		@p.price_in_cents = nil
+		@p.price_in_dollars = nil
+		refute @p.valid?
+	end
+
+	test "must refute invalid price_in_dollars" do
+		@p.price_in_cents = nil
+		@p.price_in_dollars = "abc"
+
+		assert_equal 0, @p.price_in_cents
+	end
+
+	test "updates cents value from price_in_dollars correctly" do
+		@p.price_in_dollars = 10.15
+		assert_equal 1015, @p.price_in_cents
+	end
+
+	test "must handle many decimal places price_in_dollars" do
+		@p.price_in_cents = nil
+		@p.price_in_dollars = 12.90
+
+		assert_equal 12.90, @p.price_in_dollars
+		assert_equal 1290, @p.price_in_cents
+		assert @p.valid?
+	end
+
 	test "price_in_dollars must be a float" do
 		@p.price_in_dollars = 15.00 && 15
 		assert @p.valid?
 	end
+
+
 
 	#test "price_in_dollars to price_in_cents" do
 	#	@p.price_in_dollars = price_in_cents / 100
